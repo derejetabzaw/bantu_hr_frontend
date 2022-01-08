da0b7acddf80774051c177006f0b07b8c8529497
Israel <israel.asefa.mi1055@gmail.com>
2022-01-08 05:55:29 -0800

    start from login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Register from './views/pages/Registration/registration';
+import Login from './views/pages/login/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Register></Register>
+    <Login></Login>
   )
     
 }

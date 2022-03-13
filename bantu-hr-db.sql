74e92ac196fc42cad21db1bfbb6279d56be7d246
nhattytw <nhatty567@outlook.com>
2022-03-13 17:53:42 +0300

    modified database

--- bantu-hr-db.sql
+++ bantu-hr-db.sql
@@ -1,5 +1,27 @@
 use  `bantu-hr-db`;
 
+create table areas
+(
+	areaId int NOT NULL,
+    areaCode varchar(3),
+    areaName varchar(45),
+    parentArea varchar(45),
+    remarks varchar(45),
+	PRIMARY KEY (areaId)
+);
+
+create table device
+(
+	deviceId int NOT NULL,
+	deviceName varchar(45),
+    serialNumber varchar(15),
+    ipAddress varchar(35),
+    portNumber int,
+    areaId int,
+    PRIMARY KEY (deviceID),
+    FOREIGN KEY (areaId) REFERENCES areas (areaId)
+);
+
 create table personel
 (
 	personel_id int primary key,
@@ -12,6 +34,7 @@ create table personel
     last_name varchar(45),
     job_title varchar(45),
     paygrade double,
-    image Blob
+    image Blob,
+    deviceId int,
+    FOREIGN KEY (deviceId) REFERENCES device(deviceID)
 );
-

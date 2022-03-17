f8330e88d0049d8d3c800addd888f54497de1f94
Israel <israel.asefa.mi1055@gmail.com>
2022-03-17 13:33:21 -0700

    added more tables on the database

--- bantu-hr-db.sql
+++ bantu-hr-db.sql
@@ -15,3 +15,44 @@ create table personel
     image Blob
 );
 
+create table Department
+(
+		Department_id int primary key,
+        Department_name varchar(60),
+        Parent_Department varchar(60)
+);
+
+create table Approver
+(
+	Approver_id int primary key,
+    Approver_type varchar(40),
+    image blob
+);
+
+create table Position
+(
+	position_id int primary key,
+    Deparment varchar(60),
+    parent_position varchar(60),
+    Position varchar(60),
+    position_numner int,
+    approved boolean
+);
+
+create table area 
+(
+	area_id int primary key,
+    area_code varchar(100),
+    Area_name varchar(60),
+    parent_area varchar(60),
+    remarks text
+);
+create table Device
+(
+	Device_id int primary key,
+    Device_name varchar(100),
+    Serial_number bigint,
+    ip_address varchar(32),
+    port_number int,
+    Area varchar (60)
+);

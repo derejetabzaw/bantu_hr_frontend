2705743d1e10e78c09df7ac06857a5c38daf789f
nhattytw <nhatty567@outlook.com>
2022-06-24 15:54:16 +0300

    new edit

--- zktecotest.py
+++ zktecotest.py
@@ -1,53 +1,90 @@
 from cv2 import add
+from matplotlib.pyplot import get
+from zmq import device
+
 from fpmachine.devices import ZMM220_TFT
 from fpmachine.models import UserInfo
 
-dev = ZMM220_TFT("192.168.1.105", 4370, "latin-1")
+from tabulate import tabulate
+
+ip = "192.168.1.105"
+dev = ZMM220_TFT(ip, 4370, "latin-1")
+
+
 def con():
     # create a device with ip, port and encoding
-    
+    dev.disconnect()
     dev.connect(0)
     print("connected")
 
-def conn(ip):
-    # create a device with ip, port and encoding
-    dev.connect(0)
-    print("connected")
-    
+
 def add_user(**kwargs):
     new_user = UserInfo("latin-1")
-    new_user.name = kwargs['FullName']
-    new_user.password = kwargs['password']
-    new_user.person_id = kwargs['id'] #need to sync with database  , one or more checks need to be made and photo and the fingerprint record is not required
+    new_user.name = kwargs["FullName"]
+    new_user.password = kwargs["password"]
+    new_user.person_id = kwargs[
+        "id"
+    ]  # need to sync with database  , one or more checks need to be made and photo and the fingerprint record is not required
     dev.set_user(new_user)
-ip = "192.168.1.105"
 
-def delete_user(ip, userId):
-    dev = ZMM220_TFT(ip, 4370, "latin-1")
-    dev.connect(0)
+
+def delete_user(userId):
+    con()
     dev.del_user(userId)
     print("User Deleted Successfully")
 
+
 # delete_user(ip, 1)
 
-def device_info(ip):
-    dev = ZMM220_TFT(ip, 4370, "latin-1")
-    dev.connect(0)
-    print("Connected")
 
-    print("Device Time: ", dev.device_time)
-    print("Build Version: ", dev.build_version)
-    print("Camera Open: ", dev.camera_open == '1')
-    print("Biometric type: ", dev.biometric_type)
-    print("Face Version: ", dev.zk_face_version)
-    print("Face Open: ", dev.face_fun_on == '1')
-    print("Fingerprint Version: ", dev.fp_version)
-    print("Fingerprint Open: ", dev.finger_fun_on == '1')
-    print("Mac Address: ", dev.mac_address)
-    print("Serial Number: ", dev.serial_number)
-    print("Platform: ", dev.platform)
-    print("Software Version: ", dev.software_version)
-    print("Vendor: ", dev.vendor)
-    
+def device_info():
+    con()
 
-# device_info(ip)
+    listInfo = [
+        dev.device_time,
+        dev.build_version,
+        (dev.camera_open == "1"),
+        dev.biometric_type,
+        dev.zk_face_version,
+        (dev.face_fun_on == "1"),
+        dev.fp_version,
+        (dev.finger_fun_on == "1"),
+        dev.mac_address,
+        dev.serial_number,
+        dev.platform,
+        dev.software_version,
+        dev.vendor,
+    ]
+    return listInfo
+
+
+# device_info()
+
+
+def restartDevice():
+    con()
+    dev.reboot()
+
+
+# restartDevice()
+
+
+def shutdownDevice():
+    con()
+    dev.shutdown()
+
+
+def get_users():
+    con()
+    users = dev.get_users()
+
+    userList = []
+    for user in users:
+        temp = []
+        temp.append(user.person_id)
+        temp.append(user.name)
+        temp.append(user.password)
+
+        userList.append(temp)
+
+    return userList

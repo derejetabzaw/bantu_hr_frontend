11af87ce555fa73f4859255e144f6fabb4e7b8a5
Nardos Dagnachew <nardosmehari22@gmail.com>
2023-04-26 22:40:40 +0300

    pdf

--- PdfGeneration.py
+++ PdfGeneration.py
@@ -1,19 +1,21 @@
+#this code saves the generated attendance in a form of pdf file.
+
 import fileinput
 import pandas as pd
 import numpy as np
 import os
 #import pdfkit after installing wkhtmltopdf library
 import pdfkit
-
+def PdfGeneration():
 #accepting csv input and reading it in data frame
-df = pd.read_csv('emplist.csv', encoding = "ISO-8859-1") 
+    df = pd.read_csv('emplist.csv', encoding = "ISO-8859-1") 
 
 #exporting to pdf
 #converting to html
-f = open('exp.html','w', encoding = "ISO-8859-1")
-a = df.to_html()
-f.write(a)
-f.close()
+    f = open('exp.html','w', encoding = "ISO-8859-1")
+    a = df.to_html()
+    f.write(a)
+    f.close()
 
 #converting the html to pdf
-pdfkit.from_file('exp.html', 'Attendance.pdf')
+    pdfkit.from_file('exp.html', 'Attendance10.pdf')
